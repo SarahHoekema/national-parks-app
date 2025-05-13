304ac598e1337d3668324d76a3a5d8e57731db5c
Sarah Hoekema <sarahhoekema20@gmail.com>
2025-05-13 13:16:21 -0700

    added park information dropdown

--- app.R
+++ app.R
@@ -3,33 +3,65 @@ library(bslib)
 library(ggplot2)
 library(shinythemes)
 
+#read in park and species tables
 parks <- read.csv("parks.csv", header=TRUE)
 species <- read.csv("species.csv", header=TRUE)
 
 ui <- fluidPage(
   theme = shinytheme("journal"),
+  
+  #create navigation bar with various tabs
   navbarPage(
     "US National Parks", 
+    #create conservation map tab 
     tabPanel("Conservation Map",
-      page_sidebar(
-      sidebar = sidebar(
-        checkboxGroupInput("status",
-                           "Conservation Status",
-                          choices = c("Species of Concern", "Threatened", "Endangered",
-                                     "Extinct", "In Recovery"),
-                          selected = "Species of Concern")
+      sidebarLayout(
+        sidebarPanel(
+          checkboxGroupInput("status",
+                             "Conservation Status",
+                             choices = c("Species of Concern", "Threatened", "Endangered",
+                                         "Extinct", "In Recovery"),
+                             selected = "Species of Concern")
      
-        ) 
+        ),
+        mainPanel(
+          fluidRow(
+            #print map
+          ),
+          fluidRow(
+            #print data
+          )
+        )
       )
     ),
-    tabPanel("Park Information"),
+    tabPanel("Park Information",
+      fluidRow(
+        selectizeInput("park_choice_input",
+                       "Choose a park:",
+                       choices = parks$Park.Name,
+                       options = list(
+                         placeholder = "-Select an option-",
+                         onInitialize = I("function() { this.setValue(''); }")
+                       )
+                       )
+        
+      ),
+      fluidRow(
+        column(6,
+               #create category pie chart
+               ),
+        column(6,
+               #create nativeness pie chart
+               )
+      )
+    ),
     tabPanel("Species Search")
   )
 )
 
 
 server <- function(input, output) {
-
+  
 
 }
 
