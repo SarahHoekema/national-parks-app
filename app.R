7878d86e8d668817dc9347d8f73adbe0c2ae2013
Sarah Hoekema <sarahhoekema20@gmail.com>
2025-05-19 15:44:44 -0700

    added map and viewing options

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(bslib)
 library(ggplot2)
 library(shinythemes)
+library(leaflet)
 
 #read in park and species tables
 parks <- read.csv("parks.csv", header=TRUE)
@@ -21,12 +22,15 @@ ui <- fluidPage(
                              "Conservation Status",
                              choices = c("Species of Concern", "Threatened", "Endangered",
                                          "Extinct", "In Recovery"),
-                             selected = "Species of Concern")
-     
+                             selected = "Species of Concern"),
+          selectInput("view",
+                      "Select viewing option",
+                      choices = c("Data", "Graphs"),
+                      selected = "Data")
         ),
         mainPanel(
           fluidRow(
-            #print map
+            leafletOutput("map")
           ),
           fluidRow(
             #print data
@@ -60,7 +64,20 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-  
+  output$map <- renderLeaflet({
+    leaflet() |> 
+      addTiles() |> 
+      setView(lng = -106.5348, lat = 41.850033, zoom = 3) |> 
+      addCircleMarkers(
+        data = parks,
+        lng = ~Longitude,
+        lat = ~Latitude,
+        radius = 2,
+        color = "darkgreen",
+        fillOpacity = 0.7,
+        popup = ~paste("Park: ", Park.Name)
+      )
+  })
 
 }
 
